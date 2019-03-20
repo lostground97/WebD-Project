bb094ce1caa37d61779f15fabed52a04871e41f1
Apurva101 <46521835+Apurva101@users.noreply.github.com>
2019-03-20 10:57:07 +0530

    Add files via upload

--- forgot_pswd.php
+++ forgot_pswd.php
@@ -10,9 +10,9 @@ $tbl_name="student"; // Table name
 $con = mysqli_connect("$host", "$username", "$password")or die("cannot connect"); 
 mysqli_select_db($con,"$db_name")or die("cannot select DB");
 
-$username = mysqli_escape_string($con, $_GET['username']);
-$email = mysqli_escape_string($con, $_GET['email']);
-$pswd = mysqli_escape_string($con, $_GET['password']);
+$username = mysqli_escape_string($con, $_POST['username']);
+$email = mysqli_escape_string($con, $_POST['email']);
+$pswd = mysqli_escape_string($con, $_POST['password']);
 
 $query = "SELECT * FROM student WHERE username LIKE '%$username%' and email LIKE '%$email%'";
 
@@ -24,13 +24,24 @@ if (mysqli_num_rows($result) > 0) {
      $q="update student set password='$pswd' where username='$username'";
 	 $res=mysqli_query($con,$q);
 	 if($res)
-	 {echo "password changed sucessfully";}
+	 {
+		 echo "<script>
+    alert('Password Changed Successfully.');
+    window.location.href='login.html';
+    </script>";
+    exit();
+		 
+	 }
 else{
 }
 } 
 
 else{
-   echo "invalid username or email id";
+   
+    echo "<script>
+    alert('Invald Username or Email id');
+    window.location.href='forgot_pswd.html';
+    </script>";
     exit();
 }
 
